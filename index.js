7138921a883c85e209ae972b351432dba208bad4
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-24 23:07:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 cards = ["Ada", "Brendan", "Ali"]
 birthday = "birthday"
+
 function writeCards(cards, birthday) {
   for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you, ${cards[i]}, for the wonderful ${birthday} gift!`)
   }
 }
-  
